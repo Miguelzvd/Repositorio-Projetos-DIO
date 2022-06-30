e1a2c2b50239c9c858d288144d17089ee1b45597
xXMiguelHubXx <dilsonmiguel25@gmail.com>
2022-06-30 16:00:18 -0300

    ultima atualizacao da aula

--- Projetos-DIO/javascript/Introdução-ao-javascript/aula5-contador/assets/js/script.js
+++ Projetos-DIO/javascript/Introdução-ao-javascript/aula5-contador/assets/js/script.js
@@ -1,13 +1,30 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
+var increment_ = document.getElementsByName('adicionar')[0];
+var decrement_ = document.getElementsByName('subtrair')[0];
 var currentNumber = 0;
+
+increment_.addEventListener("click", increment);
+decrement_.addEventListener("click", decrement);
+
 console.log("hello, world!")
 
 function increment(){
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    greater_than_0(currentNumber);
 }
 
 function decrement(){
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    greater_than_0(currentNumber);
+}
+
+function greater_than_0(number){
+    if (number < 0){
+        currentNumberWrapper.style.color = 'red';
+    } 
+    else{
+        currentNumberWrapper.style.color = 'white';
+    }
 }
