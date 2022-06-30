71875b8224aeebe50e71647f16a319330b3405f8
xXMiguelHubXx <dilsonmiguel25@gmail.com>
2022-06-30 01:51:33 -0300

    adicionando repositório de javascript

---
+++ Projetos-DIO/javascript/Introdução-ao-javascript/aula_introdutoria.js
@@ -0,0 +1,5 @@
+//Como comentar: utiliza-se"//" para linha unica, utiliza-se "/**/" para mais de uma linha
+//atalho para comentar: ctrl + "/"
+
+//variavel: um "objeto" que é instável, algo que é propicio a mudança (seu valor pode ser manipulado).
+//constante: um "objeto" não muda seu valor (seu valor não pode ser manipulado).
