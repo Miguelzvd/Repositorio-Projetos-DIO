39da282ae7e706d26b64bf125d2aaa02cb58ad71
xXMiguelHubXx <dilsonmiguel25@gmail.com>
2022-06-30 23:09:47 -0300

    upando projeto

---
+++ Projetos-DIO/javascript/Introdução-ao-javascript/aula6_To-do_List/README.md
@@ -0,0 +1,7 @@
+# To-do list
+## Apresentação do projeto:
+Este projeto é sobre uma lista de checkout de tarefas.
+
+### Funcionalidades:
+- O usário pode adicionar novas tarefas
+- O usário pode marcar no check-box quando uma tarefa for concluída
---
+++ Projetos-DIO/javascript/Introdução-ao-javascript/aula6_To-do_List/assets/js/script.js
@@ -0,0 +1,41 @@
+var currentNumberWrapper = document.getElementById('currentNumber');
+var increment_ = document.getElementsByName('adicionar')[0];
+var decrement_ = document.getElementsByName('subtrair')[0];
+
+var currentNumber = 0;
+var count_increment = 0;
+var count_decrement = 0;
+
+increment_.addEventListener("click", increment);
+decrement_.addEventListener("click", decrement);
+
+console.log("hello, world!")
+
+function increment(){
+    count_increment += 1;
+    
+    if (count_increment <= 10){
+        currentNumber = currentNumber + 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+        greater_than_0(currentNumber);
+    }
+}
+
+function decrement(){
+    count_decrement += 1;
+
+    if (count_decrement <= 10){
+        currentNumber = currentNumber - 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+        greater_than_0(currentNumber);
+    }
+}
+
+function greater_than_0(number){
+    if (number < 0){
+        currentNumberWrapper.style.color = 'red';
+    } 
+    else{
+        currentNumberWrapper.style.color = 'black';
+    }
+}
