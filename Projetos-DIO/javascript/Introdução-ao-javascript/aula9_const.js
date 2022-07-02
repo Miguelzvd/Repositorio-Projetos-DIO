ddbdb80a72331f2ee9b586607d5af91bdb4aaa21
xXMiguelHubXx <dilsonmiguel25@gmail.com>
2022-07-01 23:37:14 -0300

    aula 9

---
+++ Projetos-DIO/javascript/Introdução-ao-javascript/aula9_const.js
@@ -0,0 +1,9 @@
+//Uma variavel que não pode reatribuir valor
+
+const FIRST_NAME = "Miguel";
+
+console.log(FIRST_NAME);
+
+FIRST_NAME = 40; // O console ira retornar um erro devido a tentativa de reatribuição.
+
+console.log(FIRST_NAME);
