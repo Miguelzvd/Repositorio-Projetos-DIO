aee70372bf49b58ad1aefc901098f3fd81a9b001
xXMiguelHubXx <dilsonmiguel25@gmail.com>
2022-07-02 02:03:22 -0300

    aula nova

---
+++ Projetos-DIO/javascript/Introdução-ao-javascript/aula_10_Estrutura_De_Dados.js
@@ -0,0 +1,47 @@
+// Javascript é uma linguagem de tipagem dinâmica: pode mudar seu tipo de dado, exemplo: a = 1(valor númerico inteiro) -> a = "um"(valor em string).
+// Javascript é fracamente tipada: não é necessario declarar o tipo da variável.
+
+// EXEMPLO:
+
+console.log("-----------------------------------");
+let um = "um";
+
+console.log(typeof(um)); 
+
+um = 1;
+
+console.log(typeof(um));
+
+console.log("-----------------------------------");
+
+// String:
+
+let nome = "Migeul";
+
+let sobre_nome = "Azevedo";
+
+let concatenado = nome.concat(sobre_nome); //atribui a concatenização de sobre nome em nome.
+
+console.log(concatenado);
+
+console.log(concatenado[1]);
+
+console.log(concatenado.length);
+
+let frase = "Olá, tudo bem?" 
+
+console.log(frase.split(""));
+
+console.log(frase.includes("tudo"));
+
+console.log(frase.startsWith("O"));
+
+let frase_modificada = frase.replace(",", "!");
+
+console.log("\n" + frase_modificada);
+
+let exemplo = new String("blablabla");
+
+console.log(exemplo);
+
+console.log("-----------------------------------");
