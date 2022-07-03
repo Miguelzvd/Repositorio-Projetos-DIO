1fbae295036925351c754dd16745324fcc425515
xXMiguelHubXx <dilsonmiguel25@gmail.com>
2022-07-03 17:06:45 -0300

    finalizando atividade

--- Projetos-DIO/javascript/Introdução-ao-javascript/Atividade_this.js
+++ Projetos-DIO/javascript/Introdução-ao-javascript/Atividade_this.js
@@ -1,5 +1,11 @@
 function calculaIdade(anos) {
-	return `Daqui a ${anos} anos, ${this.nome} terá ${
-		this.idade + anos
-	} anos de idade.`;
+	return `Daqui a ${anos} anos, ${this.nome} terá ${this.idade + anos} anos de idade.`;
 }
+
+let pessoa = {
+    nome: "Miguel",
+    idade: 22,
+};
+
+console.log(calculaIdade.call(pessoa, 8));
+console.log(calculaIdade.apply(pessoa, [8]));
