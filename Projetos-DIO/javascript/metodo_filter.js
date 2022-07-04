0d8486a14eb85ec68c295bbbe61956622204d67f
xXMiguelHubXx <dilsonmiguel25@gmail.com>
2022-07-04 01:06:21 -0300

    aula nova

---
+++ Projetos-DIO/javascript/metodo_filter.js
@@ -0,0 +1,5 @@
+// filtra o array fazendo comparações e gerando um novo array com os elementos filtrados.
+
+const frutas = ['maçã fuji', 'mação', 'laranja', 'abacaxi'];
+
+console.log(frutas.filter((fruta) => fruta.includes('maçã')));
---
+++ Projetos-DIO/javascript/metodo_reduce.js
@@ -0,0 +1,2 @@
+// Executa uma função em todos os elementos do array, retornando um valor único.
+
