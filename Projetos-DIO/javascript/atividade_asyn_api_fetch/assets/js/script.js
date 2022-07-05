eb488084c499942ce5620d54332652c756f060a2
xXMiguelHubXx <dilsonmiguel25@gmail.com>
2022-07-05 13:16:13 -0300

    atividade api

--- Projetos-DIO/javascript/atividade_asyn_api_fetch/assets/js/script.js
+++ Projetos-DIO/javascript/atividade_asyn_api_fetch/assets/js/script.js
@@ -1,17 +1,19 @@
-const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const BASE_URL = ' https://api.thecatapi.com/v1/images/search';
 const catBtn = document.getElementById('change-cat');
+const catImg = document.getElementById('cat');
 
-const getCats = async() => {
-    try {
+const getCats =  async()=>{
+    try{
         const data = await fetch(BASE_URL);
         const json = await data.json();
-        return json.webpurl;
-    }catch(e){
-        console.log(e.message);
+        return json[0].url;
     }
-};
+    catch(e){
+        console.log(e);
+    }
+}
 
-const loadImg = async () =>{
+const loadImg = async() => {
     catImg.src = await getCats();
 }
 
