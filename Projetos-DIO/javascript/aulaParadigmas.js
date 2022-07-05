4f4f96bbc69f99f1fa6bcab937fdbd44e180a21c
xXMiguelHubXx <dilsonmiguel25@gmail.com>
2022-07-05 14:29:41 -0300

    atualização

---
+++ Projetos-DIO/javascript/aulaParadigmas.js
@@ -0,0 +1,26 @@
+// O paradigma de uma linguagem de programação é a sua identidade. Corresponde a um conjunto de 
+// características que, juntas, definem como ela opera e resolve os problema.
+
+// Orientação a objeto seus pilares:
+
+// Abstração: Processo mental que consiste em isolar um aspecto determinado de um estado de coisas
+//            relativamente complexo, a fim de simplificar a sua avaliação, classificação ou para 
+//            permitir a comunicação do mesmo.
+
+// Herança: Objeto filho herda propriedades e metodos do objeto pai, mas também tem suas próprias
+//          características.
+
+// Encapsulamento: cada classe tem propriedades e métodos independentes do resto do código. As
+//                 alterações da classe não influênciam no resto do código.
+
+// Polimorfismo: Objetos podem herdar a mesma classe pai, mas se comportam de forma diferente
+//               quando invocamos o seus métodos.
+
+// OOJS: Java Script Orientado a Objetos.
+
+// Protótipos: Todos os objeto javascript herdam propriedades e métodos de um prototype.
+//             O objeto Object.prototype está no topo desta cadeia.
+
+
+
+
