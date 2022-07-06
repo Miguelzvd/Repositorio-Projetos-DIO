a3b1b5034029307a2975516a273bb772b4fb7f82
xXMiguelHubXx <dilsonmiguel25@gmail.com>
2022-07-05 23:36:05 -0300

    arquivos novos

---
+++ Projetos-DIO/javascript/DOM_BOM.js
@@ -0,0 +1,5 @@
+// DOM: Document Object Model
+// O DOM é um padrão de como acessar e modificar elementos HTLM uem uma página.
+
+// BOM: Browser Object Model:
+// Tudo que está dentro do objeto window. é o "Pai" do DOM.
