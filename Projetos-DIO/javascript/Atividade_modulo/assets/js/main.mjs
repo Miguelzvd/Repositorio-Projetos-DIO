3dca3837db640dd49e2c35ab868bc51f4442020e
xXMiguelHubXx <dilsonmiguel25@gmail.com>
2022-07-05 23:17:49 -0300

    atividade nova

---
+++ Projetos-DIO/javascript/Atividade_modulo/assets/js/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby} from './funcoe.mjs';
+
+console.log(mostraIdade("Miguel", 22));
+console.log(mostraCidade("Miguel", "Natal"));
+console.log(mostraHobby("Miguel", "programar"));
