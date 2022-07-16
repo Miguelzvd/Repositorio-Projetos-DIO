156c2b2384b39048a322b0fd264f054ef6ad921d
miguel <dilsonmiguel25@gmail.com>
2022-07-16 20:13:26 -0300

    adicionando arquivos

--- Projetos-DIO/Angular/course-manager/src/app/courses/course-list-component.ts
+++ Projetos-DIO/Angular/course-manager/src/app/courses/course-list-component.ts
@@ -1,9 +1,41 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
 
 @Component({
     selector: 'app-course-list',
     templateUrl: './course-list-component.html' //aqui ele esta buscando o template no arquivo .html
 })
-export class CourseListComponent {
+
+//exporta a classe CoursesListComponent
+export class CourseListComponent implements OnInit { // OnInit: realiza uma operacao no momento que o componente for inicializado
+
+    //importando a classe Course sendo array, por isso eh representada como "Course[]"
+    courses: Course[] = [];//Foi criada a propriedade course sendo um array a partir da classe "Course"
+
+    //Aqui esta chamando a funcao do OnInit
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '',
+                price: 99.99,
+                code: 'XPS-8796',
+                duration: 120,
+                rating: 5.4,
+                releaseDate: 'November, 2, 2022'
+            },
+            {
+                id: 2,
+                name: 'Angular: HTTP',
+                imageUrl: '',
+                price: 45.99,
+                code: 'LKL-8796',
+                duration: 80,
+                rating: 4,
+                releaseDate: 'December, 4, 2022'
+            }
+        ];
+    };
 
 };
