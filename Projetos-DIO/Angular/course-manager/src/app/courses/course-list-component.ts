0bd1a9797f9303fd53c2edf245bad973cae015be
miguel <dilsonmiguel25@gmail.com>
2022-07-16 18:01:45 -0300

    algunas alteracoes

---
+++ Projetos-DIO/Angular/course-manager/src/app/courses/course-list-component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list-component.html' //aqui ele esta buscando o template no arquivo .html
+})
+export class CourseListComponent {
+
+};
