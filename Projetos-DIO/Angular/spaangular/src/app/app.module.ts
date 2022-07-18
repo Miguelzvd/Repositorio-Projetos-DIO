af0f63f6ae0f11bc7b92f4c4091e8f923f55bd3f
miguel <dilsonmiguel25@gmail.com>
2022-07-18 17:10:24 -0300

    novo

--- Projetos-DIO/Angular/spaangular/src/app/app.module.ts
+++ Projetos-DIO/Angular/spaangular/src/app/app.module.ts
@@ -4,12 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
 import { HomeComponent } from './home/home.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { ProfessoresComponent } from './professores/professores.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { ModelComponent } from './model/model.component';
+
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [ // para declarar componentes.
+    AppComponent,
+     HomeComponent,
+     ProfessoresComponent,
+     AlunosComponent,
+     ModelComponent
+    ], 
+  imports: [ //para declarar modulos.
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
+    ],
   providers: [],
   bootstrap: [AppComponent],
 })
