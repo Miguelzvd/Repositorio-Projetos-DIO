5571dc19e862784e3a04bfb8175f9dea0adbc92b
Miguelzvd <dilsonmiguel25@gmail.com>
2022-07-21 15:58:26 -0300

    novo

--- Projetos-DIO/Angular/crud-angular/src/app/app-routing.module.ts
+++ Projetos-DIO/Angular/crud-angular/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataBindingComponent } from './pages/data-binding/data-binding.component';
 import { TodoListComponent } from './pages/todo/todo-list/todo-list.component';
+import { PipesExampleComponent } from './pages/pipes-example/pipes-example.component';
 
 const routes: Routes = [
   {path: '', component: UsersListComponent},
   {path: 'form', component: UserFormComponent},
   {path: 'form/:id', component: UserFormComponent},
   {path: 'data-binding', component: DataBindingComponent},
-  {path: 'todo', component: TodoListComponent}
+  {path: 'todo', component: TodoListComponent},
+  {path: 'pipes', component: PipesExampleComponent}
 ];
 
 @NgModule({
